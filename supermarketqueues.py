5d560f2905c18ebd95621969a74879fac020df05
Nicole Chantzi <nicolechantzi@gmail.com>
2022-01-06 14:49:15 +0200

    first commit

--- supermarketqueues.py
+++ supermarketqueues.py
@@ -1,24 +1,4 @@
 import numpy as np
-from math import ceil
-
-def queue_time_silly(customers,n):
-    if len(customers) == 0: return 0
-    elif len(customers) <=n: return max(customers) 
-    queues = np.zeros(shape=(n,ceil(len(customers)/float(n)))).astype('int')
-    queues[:,0] = np.array([[customers[i]] for i in range(n)]).T
-    col , temp = 1 , queues[:,0].copy()
-    for c in customers[n:]:
-        if (queues[:,col] == 0).sum() == 0:
-            col += 1
-            temp = queues[:,col - 1].copy()
-        idx = np.argmin(temp)
-        queues[idx,col] = c
-        temp[idx] = temp.max() + 1
-    print(queues)
-    return np.max(queues.sum(axis=1) , axis = 0)
-
-
-import numpy as np
 
 def queue_time(customers,n):
     """Constructive solution; Recreation of the Queue Matrix"""
@@ -44,3 +24,41 @@ def main():
 
 if __name__ == "__main__": main()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def queue_time_silly(customers,n):
+#     if len(customers) == 0: return 0
+#     elif len(customers) <=n: return max(customers) 
+#     queues = np.zeros(shape=(n,ceil(len(customers)/float(n)))).astype('int')
+#     queues[:,0] = np.array([[customers[i]] for i in range(n)]).T
+#     col , temp = 1 , queues[:,0].copy()
+#     for c in customers[n:]:
+#         if (queues[:,col] == 0).sum() == 0:
+#             col += 1
+#             temp = queues[:,col - 1].copy()
+#         idx = np.argmin(temp)
+#         queues[idx,col] = c
+#         temp[idx] = temp.max() + 1
+#     print(queues)
+#     return np.max(queues.sum(axis=1) , axis = 0)
